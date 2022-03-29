864f97599badfcf19b0d9a46f62464e8f15a4800
Massimo D'Isidoro <massimo.disidoro@enea.it>
2022-03-29 10:38:20 +0200

    Modifiche di gino sulla radiazione. Su suggerimento mail di sandro del 3 marzo 2022.
    swdown=SWDOWN
    swup = SWDOWN*ALBEDO
    lwup = 5.67E-8 * TSK^4
    lwdown = GLW
    netrad=swdown - swup + lwdown - lwup
    
     	modified:   wrf_netrad.f90
     	modified:   wrf_totrad.f90

--- src/wrf_netrad.f90
+++ src/wrf_netrad.f90
@@ -9,7 +9,9 @@
 !$OMP PRIVATE(i,j)
        do j=1,south_north
        do i=1,west_east
-        NRAD(i,j)=HFX(i,j)+LH(i,j)+GRDFLX(i,j)
+!        NRAD(i,j)=HFX(i,j)+LH(i,j)+GRDFLX(i,j)
+        LWUP=EMISS(i,j)*5.67E-8*TSK(i,j)**4
+        NRAD(i,j)=SWDOWN(i,j)*(1.-ALBEDO(i,j))+LWDOWN(i,j)-LWUP
        enddo
        enddo
 !$OMP END PARALLEL DO
