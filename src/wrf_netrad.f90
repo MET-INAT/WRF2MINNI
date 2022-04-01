dbf63b6d80ca1adfdc174a16f578347843f42db2
Massimo D'Isidoro <massimo.disidoro@enea.it>
2022-04-01 10:59:55 +0200

    Corretta piccola svista in calcolo netrad=swdown - swup + lwdown - lwup
    dove è stato posto lwdwn=GLW (DOWNWARD LONG WAVE FLUX AT GROUND SURFACE)
    
     	modified:   wrf_netrad.f90

--- src/wrf_netrad.f90
+++ src/wrf_netrad.f90
@@ -1,3 +1,11 @@
+!Commento di Massimo D'Isidoro
+! Da scambio Mail di Sandro a Gino il 3/3/2022:
+!swdown=SWDOWN
+!swup = SWDOWN*ALBEDO
+!lwup = 5.67E-8 * TSK^4
+!lwdown = GLW
+!netrad=swdown - swup + lwdown - lwup
+
         subroutine wrf_netrad
         use param_wrf
         implicit none
@@ -12,7 +20,7 @@
        do i=1,west_east
 !        NRAD(i,j)=HFX(i,j)+LH(i,j)+GRDFLX(i,j)
         LWUP=EMISS(i,j)*5.67E-8*TSK(i,j)**4
-        NRAD(i,j)=SWDOWN(i,j)*(1.-ALBEDO(i,j))+LWDOWN(i,j)-LWUP
+        NRAD(i,j)=SWDOWN(i,j)*(1.-ALBEDO(i,j))+GLW(i,j)-LWUP
        enddo
        enddo
 !$OMP END PARALLEL DO
