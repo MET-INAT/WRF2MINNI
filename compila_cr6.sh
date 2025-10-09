36d70c3b34e713d72f19e43c8ad541800f5dd1f8
Massimo D'Isidoro <massimo.disidoro@enea.it>
2025-10-09 12:18:54 +0200

    Correzione in compila.sh

--- compila_cr6.sh
+++ compila_cr6.sh
@@ -1,7 +1,7 @@
 
 module load forair_lonlat
 
-pathlib=pathlib=/gporq3/minni/FORAIR_LONLAT/software/libraries/LIB_INTEL19/
+pathlib=/gporq3/minni/FORAIR_LONLAT/software/libraries/LIB_INTEL19/
 cat Makefile_CR6_ifort_tpl | sed "s!@@pathlib@@!$pathlib!g" > Makefile
 
 make 
