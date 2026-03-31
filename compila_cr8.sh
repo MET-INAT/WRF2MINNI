e7af3ceefc6e41018339208a1303d35c44210985
Massimo <massimo.disidoro@enea.it>
2026-03-31 12:32:36 +0200

    Aggiornato script compila_cr8.sh che prevede di prendere le lib di FORAIR o di CAMS

--- compila_cr8.sh
+++ compila_cr8.sh
@@ -1,6 +1,11 @@
 module load forair_portici
 
+#lib di FORAIR
 pathlib=/lpor1/store_0/usr/forair/FORAIR/OPERATIONAL/software/libraries/LIB_INTEL_ONEAPI
+
+#lib di CAMS
+#pathlib=/lpor1/store_0/usr/forecast/CAMS/software/libraries/LIB_INTEL_ONEAPI
+
 cat Makefile_CR8_ifort_tpl | sed "s!@@pathlib@@!$pathlib!g" > Makefile
 
 make 
